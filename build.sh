4b336f348da9ce30d3bd7aa97cfecb4755b448e1
Piotr Żelasko <piotr.zelasko@spoken.com>
2018-05-22 16:17:36 +0200

    Added a build script

---
+++ build.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+if [[ "$(python --version)" != "Python 3.6"* ]]; then
+    echo "ERROR: Python 3.6 required."
+    exit 1
+fi
+
+mkdir deps
+
+pushd deps
+
+OPENFST_DIR="$(pwd)/openfst_build"
+RE2_DIR="$(pwd)/re2"
+
+wget http://www.openfst.org/twiki/pub/FST/FstDownload/openfst-1.6.7.tar.gz
+tar -xvf openfst-1.6.7.tar.gz
+pushd openfst-1.6.7
+./configure --prefix="${OPENFST_DIR}" --enable-grm CXXFLAGS="-O3 -march=native"
+make -j
+make install
+popd
+
+git clone http://github.com/google/re2
+pushd re2
+git checkout 2018-03-01
+git pull
+make -j
+popd
+
+pip install -r requirements.txt
+CPATH="${OPENFST_DIR}/src/include:${RE2_DIR}" \
+LIBRARY_PATH="${OPENFST_DIR}/lib:${RE2_DIR}/obj/so" \
+python setup.py install
