c35dfc2303fb3c7fd36df600abe0ef53168386f9
Piotr Żelasko <piotr.zelasko@spoken.com>
2018-05-31 08:51:44 +0200

    Updated build script to reconfigure re2 with cmake

--- build.sh
+++ build.sh
@@ -9,45 +9,53 @@ fi
 SHARED_OBJECTS_DIR="$(pwd)/lib"
 mkdir "${SHARED_OBJECTS_DIR}"
 
+# Installations will reside here
+INSTALL_DIR="$(pwd)/installs"
+mkdir "${INSTALL_DIR}"
+OPENFST_INSTALL="${INSTALL_DIR}/openfst_build"
+RE2_INSTALL="${INSTALL_DIR}/re2"
+
 mkdir deps
 
 pushd deps
 
-OPENFST_DIR="$(pwd)/openfst_build"
-RE2_DIR="$(pwd)/re2"
+    # Compile OpenFST
+    if [ ! -f openfst-1.6.7/.compiled ]; then
+      wget http://www.openfst.org/twiki/pub/FST/FstDownload/openfst-1.6.7.tar.gz
+      tar -xvf openfst-1.6.7.tar.gz
+      pushd openfst-1.6.7
+          ./configure --prefix="${OPENFST_INSTALL}" --enable-grm CXXFLAGS="-O3 -march=native"
+          make -j
+          make install
+          touch .compiled
+      popd
+    fi
 
-# Compile OpenFST
-if [ ! -f openfst-1.6.7/.compiled ]; then
-  wget http://www.openfst.org/twiki/pub/FST/FstDownload/openfst-1.6.7.tar.gz
-  tar -xvf openfst-1.6.7.tar.gz
-  pushd openfst-1.6.7
-  ./configure --prefix="${OPENFST_DIR}" --enable-grm CXXFLAGS="-O3 -march=native"
-  make -j
-  make install
-  touch .compiled
-  popd
-fi
-
-# Compile re2
-if [ ! -f re2/.compiled ]; then
-  git clone http://github.com/google/re2
-  pushd re2
-  git checkout 2018-03-01
-  git pull
-  make -j
-  touch .compiled
-  popd
-fi
+    # Compile re2
+    if [ ! -f re2/.compiled ]; then
+      git clone http://github.com/google/re2
+      pushd re2
+          git checkout 2018-03-01
+          git pull
+          mkdir -p build
+          pushd build
+              cmake .. -DCMAKE_INSTALL_PREFIX:PATH="${RE2_INSTALL}" -DBUILD_SHARED_LIBS=TRUE -DCMAKE_BUILD_TYPE=Release
+              make -j
+              make install
+          popd
+          touch .compiled
+      popd
+    fi
 
 popd
 
 # Move all dependent *.so to one location
-cp -P ${OPENFST_DIR}/lib/*.so* "${SHARED_OBJECTS_DIR}/"
-cp -P ${RE2_DIR}/obj/so/*.so* "${SHARED_OBJECTS_DIR}/"
+cp -P ${OPENFST_INSTALL}/lib/*.so* "${SHARED_OBJECTS_DIR}/"
+cp -P ${RE2_INSTALL}/obj/so/*.so* "${SHARED_OBJECTS_DIR}/"
 
 # Build and install pynini
 pip install -r requirements.txt
-CPATH="${OPENFST_DIR}/include:${RE2_DIR}" \
+CPATH="${OPENFST_INSTALL}/include:${RE2_INSTALL}/include" \
 LIBRARY_PATH="${SHARED_OBJECTS_DIR}" \
 python setup.py install
 
