b19bbde53751281d12472001271fc4fe8a2e2e33
Piotr Żelasko <piotr.zelasko@spoken.com>
2018-07-17 08:40:45 +0200

    style

--- src/pynini_constfst.cc
+++ src/pynini_constfst.cc
@@ -5,12 +5,12 @@ namespace script {
 void ToConstFst(const FstClass &ifst_class, FstClass *ofst_class) {
 
   const auto *const std_ifst = ifst_class.GetFst<StdArc>();
-  if (std_ifst != nullptr) {
+  if (std_ifst) {
     *ofst_class = FstClass{ConstFst<StdArc, uint32_t>{*std_ifst}};
   }
 
   const auto *const pruned_wildcard_ifst = ifst_class.GetFst<PrunedWildcardArc>();
-  if (pruned_wildcard_ifst != nullptr) {
+  if (pruned_wildcard_ifst) {
     *ofst_class = FstClass{ConstFst<PrunedWildcardArc, uint32_t>{*pruned_wildcard_ifst}};
   }
 }
